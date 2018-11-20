3f80c2bb1bbc215bbab99430d74260b4881616da
Gregory Merlet <gregory.merlet@outlook.fr>
2018-11-20 13:04:29 +0100

    Chnaging socket.io on backend

--- backend/index.js
+++ backend/index.js
@@ -1,24 +1,12 @@
-const app = require('express')();
-var server = require('http').Server(app);
-var io = require('socket.io')(server);
+const io = require('socket.io')();
 
-server.listen(80);
-
-app.get('/', function (req, res) {
-	res.send('Hello World!');
+io.on('connection', (client) => {
+	client.emit('news', { hello: 'world' });
+    client.on('my other event', (data) => {
+        console.log(data);
+    });
 });
 
-app.get('/test', function (req, res) {
-	res.send('TEST');
-});
-
-app.listen(8000, function() {
-	console.log('Example app listening on port 8000!');
-});
-
-io.on('connection', function(socket) {
-	socket.emit('news', { hello: 'world' });
-	socket.on('my other event', function (data) {
-		console.log(data);
-    })
-})
+const port = 8000;
+io.listen(port);
+console.log('listening on port ', port);
