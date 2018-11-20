531cc270d37678400fa430bbf12dcc1cb5e99b68
Gregory Merlet <gregory.merlet@outlook.fr>
2018-11-20 11:27:39 +0100

    Adding socket.io to backend

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,24 @@
-const express = require('express')
-const app = express()
+const app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+server.listen(80);
 
 app.get('/', function (req, res) {
-	res.send('Hello World!')
-})
+	res.send('Hello World!');
+});
 
 app.get('/test', function (req, res) {
-	res.send('TEST')
-})
+	res.send('TEST');
+});
 
-app.listen(3000, function() {
-	console.log('Example app listening on port 3000!')
+app.listen(8000, function() {
+	console.log('Example app listening on port 8000!');
+});
+
+io.on('connection', function(socket) {
+	socket.emit('news', { hello: 'world' });
+	socket.on('my other event', function (data) {
+		console.log(data);
+    })
 })
