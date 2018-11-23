ebac0c6f0ef0b2b368cb0cffc388f764106d394c
pierrebonny <pbonny63@free.fr>
2018-11-23 07:52:52 +0100

    Jouer un tour #9 #12 #13 #14

--- pfe_tablet/src/components/WaitScreen/waitScreen.js
+++ pfe_tablet/src/components/WaitScreen/waitScreen.js
@@ -6,20 +6,47 @@ import {Label} from 'react-bootstrap';
 
 class WaitScreen extends Component{
 
-
-    componentDidMount(){
+    constructor(props){
+        super(props);
+        this.state = {
+            label: ""
+        }
 
     }
 
+    componentDidMount(){
+        this.setState ({
+           label: this.props.data.label
+        });
+        var io = require('socket.io-client');
+        const socket = io.connect(this.props.data.ip, { transports: ['websocket'], rejectUnauthorized: false });
+        socket.on('newTurn',(data) => {
+            let newData = {
+                position: this.props.data.position,
+                pseudo: this.props.data.pseudo,
+                cards: data.cards,
+                trun: data.turn,
+                age: data.age
+            };
+            this.props.changeData(newData);
+            this.props.changeComponent("HandView");
+        });
+        socket.on('gameStart',() => {
+            this.setState({
+               label: "Partie débutée, préparation du prochain tour"
+            });
+        });
+    }
+
+
     render(){
-        console.log(this.props.data);
         var divStyle = {
-            background: utils.intToColor(this.props.data.position)
+            background: "blue"//utils.intToColor(this.props.data.position)
         };
         return (
             <div id="container" style={divStyle}>
                 <Icon.Clock id="clockIcon" size={500}/>
-                <Label id="waitLabel" style={divStyle}>Bienvenue {this.props.data.pseudo} veuillez patienter</Label>
+                <Label id="waitLabel" style={divStyle}>{this.state.label}</Label>
             </div>
         );
     }
