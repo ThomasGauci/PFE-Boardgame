f161859052c26c99d834e92b371809711c592965
billy <francois.martin580@gmail.com>
2019-07-01 14:58:57 +0200

    Modification du QRView pour sélectionner la position du joueur sans passer par un QRCode

--- pfe_tablet/src/components/QRView/qrView.js
+++ pfe_tablet/src/components/QRView/qrView.js
@@ -18,7 +18,17 @@ class QrView extends Component{
 
     handleScan(data) {
         if (data) {
+            console.dir(data);
             this.props.changeData({ipAdress: data, help: false});
+            this.props.changeComponent("Join");
+        }
+    }
+
+
+    handleBtn(num) {
+        if (num && (! isNaN(num)) && ((num >= 1) && (num <=4))) {
+            this.props.changeData({ipAdress: "http://127.0.0.1:8000;"+num, help: false});
+            console.log("http://127.0.0.1:8000;"+num);
             this.props.changeComponent("Join");
         }
     }
@@ -30,6 +40,7 @@ class QrView extends Component{
     render(){
         return (
             <div id="container">
+                /*
                 {this.state.showModal ?
                     <Modal.Dialog>
                         <Modal.Body>Partie rejointe avec succès, entrez votre pseudo et choisissez votre place pour commencer la partie</Modal.Body>
@@ -40,7 +51,15 @@ class QrView extends Component{
                     </Modal.Dialog>
                     : null
                 }
+                */
                 <label id="label">Bienvenue scannez le QRCode affiché sur la table pour rejoindre la partie</label>
+                <nav>
+                    <button onClick={(e) => this.handleBtn(1)}>position 1</button>
+                    <button onClick={(e) => this.handleBtn(2)}>position 2</button>
+                    <button onClick={(e) => this.handleBtn(3)}>position 3</button>
+                    <button onClick={(e) => this.handleBtn(4)}>position 4</button>
+
+                </nav>
                 <div id="QRDiv">
                     <QrReader
                         delay={this.state.delay}
